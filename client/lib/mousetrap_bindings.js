63efae9b33759fbcea85aee58f6cca9717fe9442
Matthew Bunday <zenceph@gmail.com>
2015-01-03 00:51:08 -0500

    Refactor bindings again because TreeView isn't necessarily defined at eval time.

--- client/lib/mousetrap_bindings.js
+++ client/lib/mousetrap_bindings.js
@@ -1,10 +1,21 @@
-Mousetrap.bind('down', TreeView.focus.nextSibling);
-Mousetrap.bind('shift+down', TreeView.move.nextSibling);
-Mousetrap.bind('up', TreeView.focus.prevSibling);
-Mousetrap.bind('shift+up', TreeView.move.prevSibling);
-Mousetrap.bind('left', TreeView.focus.parent);
-Mousetrap.bind('right', TreeView.focus.children);
-
+Mousetrap.bind('down', function() {
+  TreeView.focus.nextSibling();
+});
+Mousetrap.bind('shift+down', function() {
+  TreeView.move.nextSibling();
+});
+Mousetrap.bind('up', function() {
+  TreeView.focus.prevSibling();
+});
+Mousetrap.bind('shift+up', function() {
+  TreeView.move.prevSibling();
+});
+Mousetrap.bind('left', function() {
+  TreeView.focus.parent();
+});
+Mousetrap.bind('right', function() {
+  TreeView.focus.children();
+});
 Mousetrap.bind('space', function() {
   Tree.focused().incCount(true);
   return false;
@@ -13,14 +24,15 @@ Mousetrap.bind('shift+space', function() {
   Tree.focused().incCount(true, true);
   return false;
 });
-Mousetrap.bind('ctrl+n', TreeView.handleCreateForm);
-Mousetrap.bind('ctrl+x', TreeView.removeFocused);
-
+Mousetrap.bind('ctrl+n', function() {
+  TreeView.handleCreateForm()
+});
+Mousetrap.bind('ctrl+x', function() {
+  TreeView.removeFocused();
+});
 Mousetrap.bind('ctrl+s', function() {
   $('#focus-edit-text').focus();
 });
-
 Mousetrap.bind('enter', function() {
-  var tree = Tree.focused();
-  tree.toggleFold();
+  Tree.focused().toggleFold();
 });
