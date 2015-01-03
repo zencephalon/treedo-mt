b52846a84eed2957bd3e56f74c090032a9112a4b
Matthew Bunday <zenceph@gmail.com>
2015-01-03 01:10:00 -0500

    Remove unused route.

--- router.js
+++ router.js
@@ -6,11 +6,3 @@ Router.route('/', {
     this.render("/");
   }
 });
-
-Router.route('/t/:_id/inc', function () {
-  var req = this.request;
-  var res = this.response;
-  tree = Tree.findOne(this.params._id);
-  tree.incCount(true);
-  res.end('success ' + (tree.total_count+1));
-}, {where: 'server'});
