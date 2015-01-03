58de9b6bb0ba95b90e2395699151c996b7bd8331
Matthew Bunday <zenceph@gmail.com>
2015-01-03 01:12:06 -0500

    Remove unusued count displays.

--- client/view/tree/tree.js
+++ client/view/tree/tree.js
@@ -6,28 +6,6 @@ Template.tree.events({
 });
 
 Template.tree.helpers({
-  avg_display: function(tree) {
-    today_count = tree.dayCount(new Date());
-    avg = tree.average()
-    if (today_count > avg) {
-      return "<span class='green'>" + avg + '</span>';
-    } else {
-      return avg;
-    }
-  },
-  total_yesterday_today: function(tree) {
-    yesterday_count = tree.yesterdayCount();
-    today_count = tree.todayCount();
-
-    if (today_count >= yesterday_count && today_count > 0) {
-      start_str = "<span class='green'>(</span>";
-      end_str = ")</span>";
-    } else {
-      start_str = "(";
-      end_str = "</span>)";
-    }
-    return start_str + yesterday_count + "<span class='green'>+" + today_count + end_str;
-  },
   folded_class: function(tree) {
     if (tree.folded) {
       return "folded";
